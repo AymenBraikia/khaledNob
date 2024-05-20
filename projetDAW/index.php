da0ce3ccb9e128899db1524c08cf77001dfbfa28
AymenBraikia <103582754+AymenBraikia@users.noreply.github.com>
2024-05-20 23:42:30 +0100

    Add files via upload

---
+++ projetDAW/index.php
@@ -0,0 +1,40 @@
+<?php 
+
+$conn = mysqli_connect("localhost", "root", "", "comments");
+
+if (!$conn) {
+    die("error " . mysqli_connect_error());
+}
+
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+        $comn = mysqli_real_escape_string($conn, $_POST['comn']);
+
+        $query = "INSERT INTO commentaire (email,comn) VALUES ('$email','$comn')";
+
+        if (mysqli_query($conn, $query)) {
+             header("Location:page1.php");
+            } else {
+            echo "Error: " . $query . "<br>" . mysqli_error($conn);
+        }
+
+
+mysqli_close($conn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
